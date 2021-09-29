e2d83019a31a7eabddbd858f83df30764ab1fee0
HellixITAdmin <info@hellixit.com>
2021-09-29 23:29:44 +0530

    adding java

---
+++ java/README.md
@@ -0,0 +1,2 @@
+# FoodiePal - Android app for Food Recipes
+ This android app uses APIs to get food recipes and view within the app. App is built for the submission of an assignment in final year, General Sir John Kotelawala Defence University, Sri Lanka.
---
+++ java/app/src/main/java/com/nimesh/foodsapp/view/category/CategoryFragment.java
@@ -0,0 +1,121 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 3/24/19 1:02 PM                                              -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2019. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.nimesh.foodsapp.view.category;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.nimesh.foodsapp.R;
+import com.nimesh.foodsapp.Utils;
+import com.nimesh.foodsapp.adapter.RecyclerViewMealByCategory;
+import com.nimesh.foodsapp.model.Meals;
+import com.nimesh.foodsapp.view.detail.DetailActivity;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.nimesh.foodsapp.view.home.HomeActivity.EXTRA_DETAIL;
+
+public class CategoryFragment extends Fragment implements CategoryView {
+
+    @BindView(R.id.recyclerView) 
+    RecyclerView recyclerView;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.imageCategory)
+    ImageView imageCategory;
+    @BindView(R.id.imageCategoryBg)
+    ImageView imageCategoryBg;
+    @BindView(R.id.textCategory)
+    TextView textCategory;
+    
+    AlertDialog.Builder descDialog;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (getArguments() != null) {
+            textCategory.setText(getArguments().getString("EXTRA_DATA_DESC"));
+            Picasso.get()
+                    .load(getArguments().getString("EXTRA_DATA_IMAGE"))
+                    .into(imageCategory);
+            Picasso.get()
+                    .load(getArguments().getString("EXTRA_DATA_IMAGE"))
+                    .into(imageCategoryBg);
+            descDialog = new AlertDialog.Builder(getActivity())
+                    .setTitle(getArguments().getString("EXTRA_DATA_NAME"))
+                    .setMessage(getArguments().getString("EXTRA_DATA_DESC"));
+            
+            CategoryPresenter presenter = new CategoryPresenter(this);
+            presenter.getMealByCategory(getArguments().getString("EXTRA_DATA_NAME"));
+        }
+    }
+
+    @Override
+    public void showLoading() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideLoading() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void setMeals(List<Meals.Meal> meals) {
+        RecyclerViewMealByCategory adapter = 
+                new RecyclerViewMealByCategory(getActivity(), meals);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        recyclerView.setClipToPadding(false);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        
+        adapter.setOnItemClickListener((view, position) -> {
+            TextView mealName = view.findViewById(R.id.mealName);
+            Intent intent = new Intent(getActivity(), DetailActivity.class);
+            intent.putExtra(EXTRA_DETAIL, mealName.getText().toString());
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public void onErrorLoading(String message) {
+        Utils.showDialogMessage(getActivity(), "Error ", message);
+    }
+    
+    @OnClick(R.id.cardCategory)
+    public void onClick() {
+        descDialog.setPositiveButton("CLOSE", (dialog, which) -> dialog.dismiss());
+        descDialog.show();
+    }
+    
+}
